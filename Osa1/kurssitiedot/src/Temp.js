cfd7dae93c73dc061705bc4c91fb12493d82e319
Jorma Kemppainen <j.kemppainen@kolumbus.fi>
2023-03-09 11:39:37 +0200

    Loput React sovellus tiedostot..

---
+++ Osa1/kurssitiedot/src/Temp.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+<Content part={part1} exercise={exercise1}/>
+<Content part={part2} exercise={exercise2}/>
+<Content part={part2} exercise={exercise3}/>
+<Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
